914156093235fcdc66da55ef4b292141c285b538
lazyboywu <lazyboy.wu@gmail.com>
2022-08-15 14:35:39 +0800

    添加 p1422MaximumScoreString

---
+++ src/main/java/lazyboywu/leetcode/problems/p1400_p1500/p1422MaximumScoreString/Solution.java
@@ -0,0 +1,32 @@
+package lazyboywu.leetcode.problems.p1400_p1500.p1422MaximumScoreString;
+
+/**
+ * TODO
+ *
+ * @author lazyboywu
+ * @since
+ */
+class Solution {
+    public int maxScore(String s) {
+        int score = 0;
+        int n = s.length();
+        if (s.charAt(0) == '0') {
+            score++;
+        }
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                score++;
+            }
+        }
+        int ans = score;
+        for (int i = 1; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                score++;
+            } else {
+                score--;
+            }
+            ans = Math.max(ans, score);
+        }
+        return ans;
+    }
+}
